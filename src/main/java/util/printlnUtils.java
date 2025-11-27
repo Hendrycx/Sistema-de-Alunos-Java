fa942392da3da4bb59829d05cec46ffceb88427c
Hendryc <chendrycx@gmail.com>
2025-11-27 17:07:18 -0300

    Algumas utilidades pra limpar o arquivo principal

---
+++ src/main/java/util/printlnUtils.java
@@ -0,0 +1,22 @@
+package src.main.java.util;
+
+public class printlnUtils {
+    public static void homePrintln(){
+        System.out.println("-- Bem-Vindo ao Sistema de Notas --");
+        System.out.println("1 - Entrar");
+        System.out.println("2 - Sair");
+    }
+
+    public static void mainPrintln(){
+        System.out.println("-- O que deseja fazer? --");
+        System.out.println("1 - Verificar notas de alunos");
+        System.out.println("2 - Verificar aluno");
+        System.out.println("3 - Sair");
+    }
+
+    public static void averageInputPrintln() {
+        System.out.println("-- O que deseja fazer? --");
+        System.out.println("1 - Calcular a média");
+        System.out.println("2 - Voltar");
+    }
+}
