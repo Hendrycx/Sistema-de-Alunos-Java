620608c3c17bef2ebce11851256056b585f08aaa
Hendrycx <chendrycx@gmail.com>
2025-11-29 15:34:03 -0300

    dados passados pra cá

---
+++ src/main/java/domain/AlunoData.java
@@ -0,0 +1,14 @@
+package src.main.java.domain;
+
+public class AlunoData {
+    public String name;
+    public int age;
+    public char gender;
+
+    public static String[] studentsList = {"João", "Maria", "Jose"};
+    public static double[][] notasAlunos = {{7.5, 4.3, 10.0, 9.5},
+                                            {5.5, 7.3, 8.0, 7.5},
+                                             {8.5, 3.3, 9.5, 9.5}};
+}
+
+
