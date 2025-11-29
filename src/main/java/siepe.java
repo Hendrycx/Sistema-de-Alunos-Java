9a811fa0b30ad19474afb7e4c2795e5cde700bcc
Hendrycx <chendrycx@gmail.com>
2025-11-29 15:31:16 -0300

    -lines

--- src/main/java/siepe.java
+++ src/main/java/siepe.java
@@ -1,25 +1,18 @@
 package src.main.java;
 
-import src.main.java.domain.Aluno;
-
-import java.util.Arrays;
 import java.util.Scanner;
 
-import static src.main.java.methods.studentsMethods.showStudentsList;
-import static src.main.java.util.mathUtils.calcularMedia;
-import static src.main.java.util.printlnUtils.*;
+import static src.main.java.methods.studentsMethods.verificarNotas;
+import static src.main.java.util.printlnUtils.homePrintln;
+import static src.main.java.util.printlnUtils.mainPrintln;
 
 public class siepe {
+    public static Scanner input = new Scanner(System.in);
+
     public static void main(String[] a) {
         Scanner input = new Scanner(System.in);
-
         int homeInput;
         int exitOption = 3;
-        double[] currentStudentGrade = null;
-
-        String[] students = {"João", "Maria", "Jose"};
-        double[][] notasAlunos = {{7.5, 4.3, 10.0, 9.5}, {5.5, 7.3, 8.0, 7.5}, {8.5, 3.3, 9.5, 9.5}};
-
         homePrintln(); //1-Entrar no sistema 2-Sair / Execução Única
         homeInput = input.nextInt();
         int mainInput;
@@ -27,22 +20,9 @@ public class siepe {
             do {
                 mainPrintln();
                 mainInput = input.nextInt();
-
                 switch (mainInput) {
                     case 1: //Caso escolha verificar notas dos alunos:
-                        showStudentsList(students);
-                        System.out.println("-- Qual aluno deseja verificar? --");
-                        int studentOption = input.nextInt();
-
-                        System.out.println("-- As notas foram de " + (students[studentOption - 1]) + " foram: --");
-                        System.out.println(Arrays.toString(notasAlunos[studentOption - 1]));
-                        currentStudentGrade = notasAlunos[studentOption - 1];
-
-                        averageInputPrintln(); //1- Calcular média?
-                        int averageInputNum = input.nextInt();
-                        if (averageInputNum == 1) {
-                            calcularMedia(currentStudentGrade);
-                        }
+                        verificarNotas();
                         continue;
                     default:
                         break;
